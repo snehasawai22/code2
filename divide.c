26775eb9bc436843b1e00ac45d8c9856d372ca99
snehasawai22 <140889462+snehasawai22@users.noreply.github.com>
2023-07-29 16:10:21 +0530

    Add files via upload

---
+++ divide.c
@@ -0,0 +1,50 @@
+/*#include <stdio.h>
+int main()
+{
+int a,b,add,sub,div,mul;
+printf("enter the number:");
+scanf("%d%d",&a,&b);
+add=a+b;
+sub=a-b;
+mul=a*b;
+div=a/b;
+printf("%d\n%d\n%d\n%d\n",add,sub,mul,div);
+return 0;
+}
+*/
+
+/*area of perimeter  of rectangle*/
+/*#include <stdio.h>
+int main()
+{
+int l,b,perimeter;
+scanf("%d%d",&l,&b);
+perimeter=(2*(l*b));
+printf("%d",perimeter);
+return 0;
+}
+*/
+
+/*semiperimeter of a triangle*/
+/*#include <stdio.h>
+int main()
+{
+int a,b,c,t;
+scanf("%d %d %d",&a,&b,&c);
+t=(a+b+c)/2;
+printf("%d",t);
+return 0;
+}
+*/
+
+#include<stdio.h>
+int main()
+{
+float r,area;
+float pay=3.14;
+scanf("%f",&r);
+area=((r*r)*pay);
+printf("%f.2",area);
+return 0;
+}
+
